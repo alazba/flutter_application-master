c684043ccaae94566c86d4397e8590ade85dc79d
Moza Almesaifri <moza.s@live.com>
2020-10-12 10:48:35 +0300

    Added all market list

--- WITHOUT GOOGLE SERVICE/flutter_application-master/lib/src/elements/HomeShopList.dart
+++ WITHOUT GOOGLE SERVICE/flutter_application-master/lib/src/elements/HomeShopList.dart
@@ -1,1 +1,31 @@
 
+import 'package:flutter/material.dart';
+import '../elements/HomeShopItem.dart';
+
+import '../elements/CircularLoadingWidget.dart';
+import '../models/market.dart';
+
+// ignore: must_be_immutable
+class HomeShopList extends StatelessWidget {
+  List<Market> marketsList;
+
+  HomeShopList({Key key, this.marketsList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return marketsList.isEmpty
+        ? CircularLoadingWidget(height: 200)
+        : ListView.separated(
+      padding: EdgeInsets.all(0),
+      itemBuilder: (context, index) {
+        return HomeShopItem(market: marketsList.elementAt(index));
+      },
+      separatorBuilder: (context, index) {
+        return SizedBox(height: 20);
+      },
+      itemCount: marketsList.length,
+      primary: false,
+      shrinkWrap: true,
+    );
+  }
+}
